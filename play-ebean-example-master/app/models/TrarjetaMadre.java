3d3dda367a88b5b715907a46d8378d5e66d699bf
acarreraUTC <anderson.carrera45@gmail.com>
2016-11-14 08:58:32 -0500

    atributos de la clase

--- play-ebean-example-master/app/models/TrarjetaMadre.java
+++ play-ebean-example-master/app/models/TrarjetaMadre.java
@@ -1,5 +1,23 @@
 package models;
 
-public class TrarjetaMadre {
+import com.avaje.ebean.Model;
 
+public class TrarjetaMadre extends Model{
+	
+	long id; 
+	public int precio;
+	public String modelo;
+	public int cantidad;
+	
+public TrarjetaMadre(long id, int precio, String modelo, int cantidad) {
+		super();
+		this.id = id;
+		this.precio = precio;
+		this.modelo = modelo;
+		this.cantidad = cantidad;
+	}
+	
+	
+	
+	
 }
