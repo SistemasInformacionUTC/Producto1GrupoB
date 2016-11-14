d719d282d6246105e90d151dda2c874b56404207
jtituanaUTC <judith.tituana@gmail.com>
2016-11-14 09:12:52 -0500

    importado libreriads

--- play-ebean-example-master/app/models/memoriaRam.java
+++ play-ebean-example-master/app/models/memoriaRam.java
@@ -1,5 +1,20 @@
 package models;
+import java.util.*;
+import javax.persistence.*;
+
+import com.avaje.ebean.Model;
+import play.data.format.*;
+import play.data.validation.*;
+
+import com.avaje.ebean.*;
+
+/**
+ * Computer entity managed by Ebean
+ */
+@Entity 
 
 public class memoriaRam {
 
+	
+	
 }
